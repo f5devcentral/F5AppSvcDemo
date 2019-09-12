f1f50da2c02babdd359fc29289f7d6e5aafa06e3
Robert <r.haynes@f5.com>
2019-09-12 15:39:03 -0700

    Update upload.ps1
    Added -PublicReadOnly switch

--- powershell/upload.ps1
+++ powershell/upload.ps1
@@ -1,4 +1,4 @@
-Write-S3Object -BucketName $env:DemoBkt -File .\CFT\lab.yaml
-Write-S3Object -BucketName $env:DemoBkt -File .\CFT\big-ip.yaml
-Write-S3Object -BucketName $env:DemoBkt -File .\keys\id_rsa.pem
-Write-S3Object -BucketName $env:DemoBkt -File .\keys\key.pub
+Write-S3Object -PublicReadOnly -BucketName $env:DemoBkt -File .\CFT\lab.yaml
+Write-S3Object -PublicReadOnly -BucketName $env:DemoBkt -File .\CFT\big-ip.yaml
+Write-S3Object -PublicReadOnly -BucketName $env:DemoBkt -File .\keys\id_rsa.pem
+Write-S3Object -PublicReadOnly -BucketName $env:DemoBkt -File .\keys\key.pub
